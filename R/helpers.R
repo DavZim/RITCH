1f91dcc65061b2f646348b71202c9a7aeac70d8d
DavZim <david-zimmermann@hotmail.de>
2020-06-11 15:40:19 +0200

    vectorize get_date_from_filename()

--- R/helpers.R
+++ R/helpers.R
@@ -12,13 +12,13 @@
 get_date_from_filename <- function(file) {
   date_ <- sub(".*(\\d{8}).*", "\\1", file)
   
-  if (grepl("NASDAQ_ITCH50$", file)) {
+  date_ <- ifelse(
+    grepl("NASDAQ_ITCH50(\\.gz)?$", file),
     # format MMDDYYYY
-    date_ <- gsub("(\\d{2})(\\d{2})(\\d{4})", "\\3-\\1-\\2", date_)
-  } else {
+    gsub("(\\d{2})(\\d{2})(\\d{4})", "\\3-\\1-\\2", date_),
     # format YYYYMMDD
-    date_ <- gsub("(\\d{4})(\\d{2})(\\d{2})", "\\1-\\2-\\3", date_)
-  }
+    gsub("(\\d{4})(\\d{2})(\\d{2})", "\\1-\\2-\\3", date_)
+  )
   
   date_ <- as.POSIXct(date_, tz = "GMT")
   return(date_)
