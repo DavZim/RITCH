5a24d80017d00a2783779f1587e570927233c1c7
David <david-zimmermann@hotmail.de>
2020-12-18 16:28:14 +0100

    add Msg type h to meta_data

--- R/get_meta_data.R
+++ R/get_meta_data.R
@@ -15,14 +15,14 @@
 #' @examples
 #' get_meta_data()
 get_meta_data <- function() {
-  meta <- data.table(
-    msg_type = c("S", "R", "H", "Y", "L", "V", "W", "K", "J", "A", "F", "E", 
+  data.table(
+    msg_type = c("S", "R", "H", "Y", "L", "V", "W", "K", "J", "h", "A", "F", "E", 
                  "C", "X", "D", "U", "P", "Q", "B", "I", "N"),
     msg_name = c("System Event Message", "Stock Directory", 
                  "Stock Trading Action", "Reg SHO Restriction", 
                  "Market Participant Position", "MWCB Decline Level Message",
                  "MWCB Status Message", "IPO Quoting Period Update",
-                 "LULD Auction Collar", "Add Order Message", 
+                 "LULD Auction Collar", "Operational Halt", "Add Order Message", 
                  "Add Order - MPID Attribution Message",
                  "Order Executed Message", 
                  "Order Executed Message With Price Message",
@@ -34,12 +34,11 @@ get_meta_data <- function() {
     msg_class = c("System Events", "Stock Directory",
                   "Trading Status", "Reg SHO",
                   "Market Participant States", "MWCB", "MWCB", "IPO",
-                  "LULD", "Orders", "Orders", "Orders", "Modifications", 
-                  "Modifications", "Modifications", "Modifications",   
-                  "Trades", "Trades", "Trades", "NOII", "RPII"),
+                  "LULD", "Trading Status", "Orders", "Orders", "Orders",
+                  "Modifications", "Modifications", "Modifications",
+                  "Modifications", "Trades", "Trades", "Trades", "NOII", "RPII"),
     doc_nr = c("4.1", "4.2.1", "4.2.2", "4.2.3", "4.2.4", "4.2.5.1", "4.2.5.2",
-               "4.2.6", "4.2.7", "4.3.1", "4.3.2", "4.4.1", "4.4.2", "4.4.3",
+               "4.2.6", "4.2.7", "4.2.8", "4.3.1", "4.3.2", "4.4.1", "4.4.2", "4.4.3",
                "4.4.4", "4.4.5", "4.5.1", "4.5.2", "4.5.3", "4.6", "4.7")
   )
-  return(meta)
 }
